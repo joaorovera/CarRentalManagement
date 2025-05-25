57a655411e8fb154b33819abb252a80a6da3b74c
Joao vicente <joaorovera73@gmail.com>
2025-05-25 14:50:48 -0300

    feat: create car model

---
+++ src/main/java/com/carros/todosimple/models/Car.java
@@ -0,0 +1,94 @@
+package com.carros.todosimple.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Table(name = Car.TABLE_NAME)
+public class Car {
+    public static final String TABLE_NAME = "car";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "licensePlate", nullable = false, length = 10)
+    private String licensePlate;
+
+
+    public Car() {
+    }
+
+    public Car(Long id, String model, String brand, String licensePlate) {
+        this.id = id;
+        this.model = model;
+        this.brand = brand;
+        this.licensePlate = licensePlate;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getLicensePlate() {
+        return this.licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(brand, car.brand) && Objects.equals(licensePlate, car.licensePlate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, brand, licensePlate);
+    }
+
+}
