2e7dff5a4488192ffe8933a0fad21d49234f248c
Joao vicente <joaorovera73@gmail.com>
2025-05-26 11:23:51 -0300

    refactor: add @repository in repositorie files

--- src/main/java/com/carros/rentalcar/repositories/CarRepository.java
+++ src/main/java/com/carros/rentalcar/repositories/CarRepository.java
@@ -2,8 +2,11 @@ package com.carros.rentalcar.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.carros.rentalcar.models.Car;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
     List<Car> findByModel(String model);
 }
